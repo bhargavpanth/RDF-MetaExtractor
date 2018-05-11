eeccb29e35a8d59ef212a9ef9a611d8dcf6d78f4
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-11 20:20:54 +0100

    add number of instance to the class

--- src/lib/instance/instance.py
+++ src/lib/instance/instance.py
@@ -5,39 +5,41 @@ import sys
 class InstanceDataExtractor(object):
 	
 	"""
-	MetaData class returns s,p,o from the dataset
+	InstanceDataExtractor class returns s,p,o from the dataset
 	
 	"""
-	
+
 	def __init__(self, fname):
 		self.fname = fname
 
-	def get_instances(self):
+
+	def get_instances(self, num):
 		if os.path.exists(os.path.join('data/', self.fname)):
 			print 'parsing contents from ' + self.fname + '...'
-			content = '' 
+			content = ''
 			graph = rdflib.Graph()
 			with open(os.path.join('data/', self.fname)) as dataset:
 				for contents in dataset:
-					# append contents into a string
 					content += contents
-			
+					# append contents into a string
+
 			graph.parse(data=content)
 			output = []
 			for sub, pred, obj in graph:
+				if type(obj) == rdflib.term.Literal:
+					output.append(obj.toPython())
 				'''
 				Subject type : rdflib.term.URIRef
 				Predicate type : rdflib.term.URIRef
 				Object type : rdflib.term.Literal
 				'''
-				if type(obj) == rdflib.term.Literal:
-					output.append(obj.toPython())
-			return output
+			return output[0:num]
 		else:
 			print 'dataset does not exist - check for filename'
 
 def main():
-	test = InstanceDataExtractor('sample.rdf').get_instances()
+	test = InstanceDataExtractor('sample.rdf').get_instances(2)
+	print test
 
 if __name__ == '__main__':
 	main()
