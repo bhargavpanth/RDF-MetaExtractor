a2c588ff89bc5f382ec770fd63eddb020506b08e
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-11 16:32:13 +0100

    remove virtual env

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# switch to virtual env
+$VENV='virtualenv ENV'
+$INIT='source ENV/bin/activate'
+
 # install dependencies
-PIP=''
+PIP='which pip'
 DEP='install requirements.txt'
 
 $PIP $DEP
