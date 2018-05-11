cb38ffbb126009a9d26466eecf204d86d0f88117
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-11 19:56:56 +0100

    read and parse file

--- src/lib/metadata/metadata.py
+++ src/lib/metadata/metadata.py
@@ -1,33 +1,40 @@
 import os
 import rdflib
+import sys
 
 class MetaDataExtractor(object):
+	
 	"""
-	docstring for MetaData
+	MetaData class returns s,p,o from the dataset
+	
 	"""
+	
 	def __init__(self, fname):
 		self.fname = fname
 
 	def parse_tags(self):
-		if os.path('../../../data/', self.fname):
-			# dataset exists
+		
+		if os.path.exists(os.path.join('data/', self.fname)):
 			print 'parsing contents from ' + self.fname + '...'
-			with open(os.path('../../../data/', self.fname)) as dataset:
+			content = '' 
+			with open(os.path.join('data/', self.fname)) as dataset:
 				for contents in dataset:
-					print contents
 					# append contents into a string
-					# graph = rdflib.Graph()
-					# graph.parse(data=dataset)
-					# output = []
-					# for s, p, o in graph:
-					#     if type(o) == rdflib.term.Literal:
-					#         output.append(o.toPython())
-					# print ', '.join(output)
+
+					
+			graph = rdflib.Graph()
+			graph.parse(data=dataset)
+			output = []
+			for s, p, o in graph:
+				if type(o) == rdflib.term.Literal:
+					output.append(o.toPython())
+			print ', '.join(output)
+
 		else:
 			print 'dataset does not exist - check for filename'
 
 def main():
-	pass
+	test = MetaDataExtractor('sample.rdf').parse_tags()
 
 if __name__ == '__main__':
 	main()
