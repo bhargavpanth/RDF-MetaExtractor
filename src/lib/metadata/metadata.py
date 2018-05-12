97e782f0044bd7908b21461be36b93fa584d9f4b
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-12 18:26:54 +0100

    get namespaces from the dataset - use it to analyse the meta data quality

--- src/lib/metadata/metadata.py
+++ src/lib/metadata/metadata.py
@@ -2,6 +2,7 @@ import os
 import rdflib
 import sys
 import xml.etree.ElementTree as ET
+from io import StringIO
 
 '''
 -----------------------------------------
@@ -27,12 +28,11 @@ rdf:Description
 Void: Dataset
 Void: sparlqlEndpoint
 Void: urilookupendpoint
+void:vocabulary
 
 dataid:identifierScheme
 dataid:dataDescription
 dataid:similarData
-
-void:vocabulary
 
 dublinCore: Description
 dublinCore: Identifier
@@ -56,42 +56,48 @@ class MetaDataExtractor(object):
 	def __init__(self, fname):
 		self.fname = fname
 
+
+	# list the namespaces in the RDF file
+	def get_namespaces(self):
+		namespaces = dict([node for _, node in ET.iterparse(os.path.join('data/', self.fname), events=['start-ns'])])
+		return namespaces
+
+
 	def get_metadata(self):
 		g = rdflib.Graph()
 		file = os.path.join('data/', self.fname)
 		if os.path.exists(file):
 			print 'parsing contents from ' + self.fname + '...'
-			'''
-			RDF(S) datasets are XML files - parse tags from the list
-			'''
+			# RDF(S) datasets are XML files - parse tags from the list
 			tree = ET.parse(file)
 			root = tree.getroot()
+			# list all namespaces to be checked - datasets with unconventional shorthands or names will induce warnings
+			namespace_list = [{'rdf' : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'}, {'rdfs' : 'http://www.w3.org/2000/01/rdf-schema#'}, {'skos' : 'http://www.w3.org/2008/05/skos#'}, {'dcat' : 'http://www.w3.org/ns/dcat#'}, {'dct' : 'http://purl.org/dc/terms/'}, {'dctype' : 'http://purl.org/dc/dcmitype/'}, {'dc'}, {'void'}, {'dataid'}, {'dbp' : 'http://dbpedia.org/ontology/'}, {'dbprop' : 'http://dbpedia.org/property/'}, {'foaf' : 'http://xmlns.com/foaf/0.1/'}]
 			namespaces = {'rdf' : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'}
-			print root.findall('rdf:Description', namespaces)
-			# print root.findall('rdf:Description')
-			return root
-
-			'''
-			# querying the RDF dataset
-			g.parse(file)
-			qres = g.query(
-				"""SELECT DISTINCT ?aname ?bname
-				WHERE {
-				?a foaf:knows ?b .
-				?a foaf:name ?aname .
-				?b foaf:name ?bname .
-			}""")
-		
-			for row in qres:
-			    print("%s knows %s" % row)
-			'''
+			return root.findall('rdf:Description', namespaces)
 		else:
 			print 'dataset does not exist - check for filename'
 
+
 def main():
-	test = MetaDataExtractor('sample.rdf').get_metadata()
+	# test = MetaDataExtractor('sample.rdf').get_metadata()
+	test = MetaDataExtractor('udvietnamese.rdf').get_namespaces()
 	print test
 
 if __name__ == '__main__':
 	main()
 
+
+'''
+# querying the RDF dataset
+g.parse(file)
+qres = g.query(
+	"""SELECT DISTINCT ?aname ?bname
+	WHERE {
+	?a foaf:knows ?b .
+	?a foaf:name ?aname .
+	?b foaf:name ?bname .
+	}""")
+	for row in qres:
+		print("%s knows %s" % row)
+'''
